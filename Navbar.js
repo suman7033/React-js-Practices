49e87612786287bfde4763c7f48634427b52316c
suman kumar <133625451+suman7033@users.noreply.github.com>
2023-07-15 14:16:34 +0530

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+
+class Navbar extends Component {
+  state = {};
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Apna_Cart
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
