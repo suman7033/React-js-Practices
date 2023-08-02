08780908879955d3947b15acb4c1356378639f59
suman kumar <133625451+suman7033@users.noreply.github.com>
2023-08-02 22:24:32 +0530

    No match data so 'no Found'

--- Filter_Data.js
+++ Filter_Data.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
-import React, { useEffect,useState } from 'react'
+import React, {useEffect,useState} from 'react'
 
 export default function Filter_Data() {
     const [data,setData]=useState([])
     const [records,setRecords]=useState(data)
+    const [noResults,setNoResults]=useState(false);
     useEffect(()=>{
         axios.get('https://jsonplaceholder.typicode.com/users')
         .then(res => {
@@ -12,13 +13,29 @@ export default function Filter_Data() {
         })
         .catch(err => console.log(err));
     },[])
-    const filter=(e)=>{
-      setRecords(data.filter(f =>f.name.toLowerCase().includes(e.target.value)))
-    }
+    // const filter=(e)=>{
+        const filter = (e) => {
+            const inputValue = e.target.value.toLowerCase();
+            const filteredData = data.filter((f) =>
+              f.name.toLowerCase().includes(inputValue)
+            );
+          
+            if (filteredData.length === 0) {
+              setNoResults(true);
+            } else {
+              setNoResults(false);
+              setRecords(filteredData);
+            }
+          };
+          
+    //   setRecords(data.filter(f =>f.name.toLowerCase().includes(e.target.value)))
+    //}
   return (
-    <div className='p-5 bg-light'>
-    <div className='bg-white shadow border'>
-        <input type='text' onChange={filter} placeholder='filter'/>
+    <div className='container mt-5'>
+        <input type='text' className='text-warning bg-black shadow border'onChange={filter}placeholder='filter'/><br></br>
+        <button onClick={filter}>Filter</button><br></br>
+    <div className='bg-warning shadow border'><br></br>
+    {noResults ? <h2>No Found</h2> : null}
     <table className='table'>
         <thead>
             <tr>
